092f0e4232610e0fc27c8858f5222961150bfea2
YunChan-Oh <oyunchan9@gmail.com>
2024-11-24 02:04:34 +0900

    init

---
+++ src/main/java/com/moghwa/server/MoghwaServerApplication.java
@@ -0,0 +1,13 @@
+package com.moghwa.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoghwaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoghwaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moghwa/server/MoghwaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moghwa.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoghwaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
